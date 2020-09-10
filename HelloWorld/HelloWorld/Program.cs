2cf4e600acb434bc18f2e44e39c669ab0a15f1d9
gracearnsberger <50274639+gracearnsberger@users.noreply.github.com>
2020-09-10 14:58:10 -0600

    Initial commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GraceProgram
+
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accouting Systems");
+            Console.WriteLine("Remember, we're 'accounting' on you!");
+            Console.ReadLine();
+        }
+    }
+}
