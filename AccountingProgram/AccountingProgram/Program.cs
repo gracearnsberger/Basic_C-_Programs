59f3bb343001d7742df0c7c041d3da29fd8339a2
gracearnsberger <50274639+gracearnsberger@users.noreply.github.com>
2020-09-14 11:20:19 -0600

    Acme Accounting Systems
    Simple C# console display

---
+++ AccountingProgram/AccountingProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AccountingProgram
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accouting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
