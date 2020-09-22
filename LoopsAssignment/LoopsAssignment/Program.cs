3f561cce775cc7943730d8da90cea680c1b7576b
gracearnsberger <50274639+gracearnsberger@users.noreply.github.com>
2020-09-22 10:11:35 -0600

    Loops Assignment
    Loop using while and while do in C#

---
+++ LoopsAssignment/LoopsAssignment/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsProgram
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Guess what number of the month is my birthday!?");
+                int number = Convert.ToInt32(Console.ReadLine());
+                bool isGuessed = number == 22;
+
+                do
+                {
+                    switch (number)
+                    {
+                        case 20:
+                            Console.WriteLine("You guessed the 20th. You're close but not right.");
+                            Console.WriteLine("Guess a number?");
+                            number = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 10:
+                            Console.WriteLine("You guessed the 10th. Try higher.");
+                            Console.WriteLine("Guess a number?");
+                            number = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 30:
+                            Console.WriteLine("You guessed the 30th. Try lower.");
+                            Console.WriteLine("Guess a number?");
+                            number = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 22:
+                            Console.WriteLine("You guessed the 22nd! You're correct!");
+                            isGuessed = true;
+                            break;
+                        default:
+                            Console.WriteLine("You are wrong. Think middle of the month.");
+                            Console.WriteLine("Guess a number?");
+                            number = Convert.ToInt32(Console.ReadLine());
+                            break;
+                    }
+                }
+
+                while (!isGuessed);
+
+                Console.Read();
+            }
+        }
+    }
+}
+    }
+}
