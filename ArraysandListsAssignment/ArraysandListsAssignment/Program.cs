75ccca44976e1ab09631416499824657ca3352a2
gracearnsberger <50274639+gracearnsberger@users.noreply.github.com>
2020-09-23 13:26:13 -0600

    Array Assignment

--- ArraysandListsAssignment/ArraysandListsAssignment/Program.cs
+++ ArraysandListsAssignment/ArraysandListsAssignment/Program.cs
@@ -4,35 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ArraysandListsAssignment
+namespace ArrayAssignment
 {
     class Program
     {
         static void Main(string[] args)
         {
-            List<string> intList = new List<string>();
+            //define array 
+            //string[] sodas = { "Fanta", "Coke", "Sprite", "Root Beer" };
+            //Console.WriteLine("Enter an index number to choose your soda: ");
+            //int input = Convert.ToInt32(Console.ReadLine());
 
-            intList.Add("Hello");
-            intList.Add("grace");
-            intList.Remove("grace");
+            ////if statement for if the index is out of range
+            //if (input>sodas.Length)
+            //{
+            //    Console.WriteLine("Please choose a number inside of the array such as 1-4.");
+            //    Console.ReadLine();
+            //}
 
-            Console.WriteLine(intList[0]);
-            Console.ReadLine();
-            //int[] numArray = new int[5];
-            //numArray[0] = 5;
-            //numArray[1] = 2;
-            //numArray[2] = 10;
-            //numArray[3] = 200;
-            //numArray[4] = 5000;
-
-            //int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 };
-
-            //int[] numArray2 = new int[] { 5, 2, 10, 200, 5000, 600, 23000 };
-
-            //numArray2[5] = 650;
-
-            //Console.WriteLine(numArray2[5]);
+            //Console.WriteLine("The soda at index number: " + input + " is " + sodas[input]);
             //Console.ReadLine();
+
+            //define int array
+            //int[] intArray = { 30, 22, 12, 100 };
+            //Console.WriteLine("Pick an index number to get your number.");
+            //int input2 = Convert.ToInt32(Console.ReadLine());
+
+            ////if statement for if the index is out of range
+            //if (input2>intArray.Length)
+            //{
+            //    Console.WriteLine("You have chosen an index number out of range. Choose 1-4.");
+            //    Console.ReadLine();
+            //}
+
+            //Console.WriteLine("The integer at index number" + input2 + " is " + intArray[input2]);
+            //Console.ReadLine();
+
+            //create list of strings
+            List<string> stringList = new List<string>() { "Denver", "Colorado Springs", "Aspen" };
+            Console.WriteLine("Enter an index number to get your Colorado city.");
+            int input3 = Convert.ToInt32(Console.ReadLine());
+
+            //if statement for if the index is out of range
+            if(input3 > stringList.Count)
+            {
+                Console.WriteLine("You have picked a number that is out range. Pick a number 1-3.");
+                Console.ReadLine();
+            }
+                Console.WriteLine("The city at index number is: " + input3 + " is " + stringList[input3]);
+            Console.ReadLine();
         }
     }
 }
