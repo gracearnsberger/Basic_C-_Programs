d071027b9449b4f1323c88288f66e7f3b487e544
gracearnsberger <50274639+gracearnsberger@users.noreply.github.com>
2020-09-23 09:17:24 -0600

    Strings Assignment
    Concatenate 3 strings, convert a string to all upper case, and use sb to build a paragraph using one sentence at a time.

---
+++ StringsAssignmet/StringsAssignmet/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsAssignmet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Concatenate 3 strings
+            string name = "grace!";
+            string greeting = "Hello! ";
+            string middle = "my name is ";
+
+            string str = greeting + middle + name;
+
+            //Converted a string to uppercase letters
+            string state = "Colorado";
+            state = state.ToUpper();
+
+
+            //Used StringBuilder to build a paragraph of text, one sentence at a time.
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("\t My name is grace. ");
+            sb.Append("I am from Denver Colorado. ");
+            sb.Append("But I also lived in Austin. ");
+            sb.Append("\n I work as an office manager for a startup software company. ");
+            sb.Append("I want to make it as a software engineer. ");
+            sb.Append("And maybe one day move to Europe. ");
+
+            Console.WriteLine(str);
+            Console.WriteLine(state);
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
