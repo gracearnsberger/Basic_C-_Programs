92d9557d99d226c2482292b027fe7bd06cc0d855
gracearnsberger <50274639+gracearnsberger@users.noreply.github.com>
2020-09-23 13:19:31 -0600

    Array Assignment

--- LoopsAssignment/LoopsAssignment/Program.cs
+++ LoopsAssignment/LoopsAssignment/Program.cs
@@ -5,100 +5,99 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-//namespace WhileStatement
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Guess my number between 1 & 10.");
-//            int numberGuess = Convert.ToInt32(Console.ReadLine());
-//            bool guessedNumber = false;
-//              
-//            while (guessedNumber == false);
-//            
-//            
-//                switch (numberGuess)
-//                {
-//                    case 2:
-//                        Console.WriteLine("You guessed 2. You're not right.");
-//                        Console.WriteLine("Guess a number?");
-//                        number = Convert.ToInt32(Console.ReadLine());
-//                        break;
-//                    case 8:
-//                        Console.WriteLine("You guessed 8. You're not right.");
-//                        Console.WriteLine("Guess a number?");
-//                        number = Convert.ToInt32(Console.ReadLine());
-//                        break;
-//                    case 5:
-//                        Console.WriteLine("You guessed 5. Try higher.");
-//                        Console.WriteLine("Guess a number?");
-//                        number = Convert.ToInt32(Console.ReadLine());
-//                        break;
-//                    case 7:
-//                        Console.WriteLine("You guessed 7! You're correct!");
-//                        isGuessed = true;
-//                        break;
-//                    default:
-//                        Console.WriteLine("You are wrong. Think middle of the numbers.");
-//                        Console.WriteLine("Guess a number?");
-//                        number = Convert.ToInt32(Console.ReadLine());
-//                        break;
-//                }
-//            }
-
-//            Console.Read();
-//        }
-//    }
-//}
-
-namespace LoopsProgram
+namespace WhileStatement
+{
+    class Program
     {
-        class Program
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
+            Console.WriteLine("Guess my number between 1 & 10.");
+            int numberGuess = Convert.ToInt32(Console.ReadLine());
+            bool guessedNumber = false;
+
+            while (guessedNumber == false);
+
+
+            switch (numberGuess)
             {
-                Console.WriteLine("Guess what number of the month is my birthday!?");
-                int number = Convert.ToInt32(Console.ReadLine());
-                bool isGuessed = number == 22;
-
-                do
-                {
-                    switch (number)
-                    {
-                        case 20:
-                            Console.WriteLine("You guessed the 20th. You're close but not right.");
-                            Console.WriteLine("Guess a number?");
-                            number = Convert.ToInt32(Console.ReadLine());
-                            break;
-                        case 10:
-                            Console.WriteLine("You guessed the 10th. Try higher.");
-                            Console.WriteLine("Guess a number?");
-                            number = Convert.ToInt32(Console.ReadLine());
-                            break;
-                        case 30:
-                            Console.WriteLine("You guessed the 30th. Try lower.");
-                            Console.WriteLine("Guess a number?");
-                            number = Convert.ToInt32(Console.ReadLine());
-                            break;
-                        case 22:
-                            Console.WriteLine("You guessed the 22nd! You're correct!");
-                            isGuessed = true;
-                            break;
-                        default:
-                            Console.WriteLine("You are wrong. Think middle of the month.");
-                            Console.WriteLine("Guess a number?");
-                            number = Convert.ToInt32(Console.ReadLine());
-                            break;
-                    }
-                }
-
-                while (!isGuessed);
-
-                Console.Read();
+                case 2:
+                    Console.WriteLine("You guessed 2. You're not right.");
+                    Console.WriteLine("Guess a number?");
+                    numberGuess = Convert.ToInt32(Console.ReadLine());
+                    break;
+                case 8:
+                    Console.WriteLine("You guessed 8. You're not right.");
+                    Console.WriteLine("Guess a number?");
+                    numberGuess = Convert.ToInt32(Console.ReadLine());
+                    break;
+                case 5:
+                    Console.WriteLine("You guessed 5. Try higher.");
+                    Console.WriteLine("Guess a number?");
+                    numberGuess = Convert.ToInt32(Console.ReadLine());
+                    break;
+                case 7:
+                    Console.WriteLine("You guessed 7! You're correct!");
+                    guessedNumber = true;
+                    break;
+                default:
+                    Console.WriteLine("You are wrong. Think middle of the numbers.");
+                    Console.WriteLine("Guess a number?");
+                    numberGuess = Convert.ToInt32(Console.ReadLine());
+                    break;
             }
+                Console.Read();
         }
     }
+}
+
+
+//namespace LoopsProgram
+//    {
+//        class Program
+//        {
+//            static void Main(string[] args)
+//            {
+//                Console.WriteLine("Guess what number of the month is my birthday!?");
+//                int number = Convert.ToInt32(Console.ReadLine());
+//                bool isGuessed = number == 22;
+
+//                do
+//                {
+//                    switch (number)
+//                    {
+//                        case 20:
+//                            Console.WriteLine("You guessed the 20th. You're close but not right.");
+//                            Console.WriteLine("Guess a number?");
+//                            number = Convert.ToInt32(Console.ReadLine());
+//                            break;
+//                        case 10:
+//                            Console.WriteLine("You guessed the 10th. Try higher.");
+//                            Console.WriteLine("Guess a number?");
+//                            number = Convert.ToInt32(Console.ReadLine());
+//                            break;
+//                        case 30:
+//                            Console.WriteLine("You guessed the 30th. Try lower.");
+//                            Console.WriteLine("Guess a number?");
+//                            number = Convert.ToInt32(Console.ReadLine());
+//                            break;
+//                        case 22:
+//                            Console.WriteLine("You guessed the 22nd! You're correct!");
+//                            isGuessed = true;
+//                            break;
+//                        default:
+//                            Console.WriteLine("You are wrong. Think middle of the month.");
+//                            Console.WriteLine("Guess a number?");
+//                            number = Convert.ToInt32(Console.ReadLine());
+//                            break;
+//                    }
+//                }
+
+//                while (!isGuessed);
+
+//                Console.Read();
+//            }
+//        }
+//    }
 
     
 
