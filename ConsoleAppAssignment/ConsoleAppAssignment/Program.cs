df163d7be8fbf469716d70276bc87104c77df271
gracearnsberger <50274639+gracearnsberger@users.noreply.github.com>
2020-09-28 08:38:46 -0600

    Console App Assignment

--- ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
+++ ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -70,61 +70,102 @@ namespace ConsoleAppAssignment
             //Console.Read();
 
             //create unique strings list
-            Console.WriteLine("colors list");
+            bool myList;
+            bool rational = false;
+            string cont, userChoice;
             List<string> colors = new List<string>() { "Pink", "Blue", "Orange", "Green", "Blue", "Yellow", "Black", "White", "Black", "Purple" };
-            Console.WriteLine("Colors:");
-            Console.WriteLine("Choose a color for indicies.");
-            foreach (string c in colors)
-            {
-                Console.WriteLine(c);
-            }
-            Console.WriteLine();
             //ask the user to input text to search for in the list & print index
-            Console.WriteLine("Enter the name of your color");
-            Console.WriteLine();
-            string mColor = Console.ReadLine();
-
-            int yNum = 0;
-            bool yColor = false;
-            List<int> yIndex = new List<int>();
-
-            Console.WriteLine();
-
+            Console.WriteLine("Select a color & I will display the index numbers.");
+            int i = 0;
             foreach (string c in colors)
             {
-                if (c.Equals(mColor))
-                {
-                    yIndex.Add(yNum);
-                    yColor = true;
-                }
-                yNum++;
+                Console.WriteLine(c + " " + i);
+                i++;
             }
-            Console.ReadLine();
+            //while (!rational)
+            //    userChoice = Console.ReadLine().ToLower();
+                //tell a user if they put in text that isn’t in the list.
+                //myList = colors.Exists(x => x.ToLower() == userChoice);
+                //if (myList)
+                //{
+                //    rational = true;
+                //    for (int i = 0; i < colors.Count; i++)
+                //    {
+                //        if (userChoice == colors[i].ToLower())
+                //        {
+                //            Console.WriteLine("The index of the color you selected is: " + i);
+                //            break;
+                //        }
+                //    }
+                //}
+                //else
+                //{
+                //    Console.WriteLine("You did not pick a color from my list.");
+                //    Console.WriteLine("Would you like to try again? (yes/no)?");
+                //    cont = Console.ReadLine().ToLower();
+                //    if (cont == "no") { rational = true; }
+
+                //}
+                    Console.Read();
+        }
+
+            
 
             //list of strings with two identical words
-            //Console.WriteLine("List of pets");
-            //List<string> pets = new List<string>() { "cat", "dog", "fish", "lizard", "bird", "dog", "chicken", "spider", "snake", "hamster" };
-            //List<string> samePets = new List<string>();
-            //StringBuilder indicies = new StringBuilder();
-            ////iterates through the list and then displays the indices of the list that contain matching text on the screen.
-            //StringBuilder petsDisplay = new StringBuilder("[");
-            //foreach (string p in pets)
-            //{
-            //    petsDisplay.Append(" " + p + ",");
-            //    if (samePets.Exists(x => x == p))
-            //    {
-            //        Console.WriteLine(p + " has already appeared in the list");
-            //    }
-            //    else
-            //    {
-            //        samePets.Add(p);
-            //        Console.WriteLine(p + " has not appeared on the list before");
-            //    }
-            //}
-            //petsDisplay.Length = (petsDisplay.Length - 1);
-            //petsDisplay.Append("]");
-            //Console.WriteLine("Pets List" + petsDisplay);
-            //Console.Read();
-        }
+        //    Console.WriteLine("List of pets");
+        //    List<string> pets = new List<string>() { "cat", "dog", "fish", "lizard", "bird", "dog", "chicken", "spider", "snake", "hamster" };
+        //    List<string> samePets = new List<string>();
+        //    StringBuilder indicies = new StringBuilder();
+        //    //iterates through the list and then displays the indices of the list that contain matching text on the screen.
+        //    StringBuilder petsDisplay = new StringBuilder("[");
+        //    foreach (string p in pets)
+        //    {
+        //        petsDisplay.Append(" " + p + ",");
+        //        if (samePets.Exists(x => x == p))
+        //        {
+        //            Console.WriteLine(p + " has already appeared in the list");
+        //        }
+        //        else
+        //        {
+        //            samePets.Add(p);
+        //            Console.WriteLine(p + " has not appeared on the list before");
+        //        }
+
+        //    }
+        //    petsDisplay.Length = (petsDisplay.Length - 1);
+        //    petsDisplay.Append("]");
+        //    Console.WriteLine("Pets List" + petsDisplay);
+
+        //    //user input 
+        //    rational = false; 
+        //    while (!rational)
+        //    {
+        //        Console.WriteLine("Select a pet from the list to display the indicies:");
+        //        userChoice = Console.ReadLine().ToLower();
+        //        myList = pets.Exists(x => x.ToLower() == userChoice);
+
+        //        if (myList)
+        //        {
+        //            rational = true;
+        //            for (int i = 0; i < pets.Count; i++)
+        //            {
+        //                if (userChoice == pets[i].ToLower())
+        //                {
+        //                    indicies.Append(i + ",");
+        //                }
+        //            }
+        //            indicies.Length = (indicies.Length - 2);
+        //            Console.WriteLine("The pet \"{0}\" occurs in the list at the index of: {1}", userChoice, indicies);
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("You did not pick a pet from the list.");
+        //            Console.WriteLine("Would you like to try again? (yes/no)?");
+        //            cont = Console.ReadLine().ToLower();
+        //            if (cont == "no") { rational = true; }
+        //        }
+        //    }
+        //    Console.Read();
+        //}
     }
 }
