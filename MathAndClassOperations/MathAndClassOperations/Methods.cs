4ab0829f284aaa4b91a3601ad2e161cf5aaaa17f
gracearnsberger <50274639+gracearnsberger@users.noreply.github.com>
2020-09-28 17:22:01 -0600

    Methods and Class Practice
    Use 3 methods to perform math operations on a user entered number. In the main program call each methos in turn

---
+++ MathAndClassOperations/MathAndClassOperations/Methods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndClassOperations
+{
+    public class Methods
+    {
+        //create 3 methods, which will take one integer parameter in and return an integer
+        //method 1, division
+        public int division(int int1, int int2)
+        {
+            var Total = int1 / int2;
+            return Total;
+        }
+        //method 2, multiplication
+        public int multiplication(int int1, int int2)
+        {
+            var Total = int1 * int2;
+            return Total;
+        }
+        //method 3, addition
+        public int addition(int int1, int int2)
+        {
+            var Total = int1 + int2;
+            return Total;
+        }
+    }
+}
---
+++ MathAndClassOperations/MathAndClassOperations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndClassOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask the user what number they want to do the math operations on.
+            Console.WriteLine("Please enter a number(#) of your choice. :) ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = 2;
+            int Total;
+            Methods myFunction = new Methods();
+
+            //call each method in turn, passing the user input to the method. 
+            //and display the returned integer to the screen
+
+            Total = myFunction.division(x, y);
+            Console.WriteLine("The total of " + x + " divided by " + y + " equals " + Total);
+            Total = myFunction.multiplication(x, y);
+            Console.WriteLine("The total of " + x + " multiplied by " + y + " equals " + Total);
+            Total = myFunction.addition(x, y);
+            Console.WriteLine("The total of " + x + " added to " + y + " equals " + Total);
+            Console.WriteLine("Thanks for using my calculator!");
+            Console.ReadLine();
+
+
+            
+        }
+    }
+}
