4facb50ecb9923e63a16fee471ff4a6509b0cb2a
gracearnsberger <50274639+gracearnsberger@users.noreply.github.com>
2020-09-29 09:30:15 -0600

    Inheritance
    Practice using inheritance from classes using c#.

---
+++ Inheritance/Inheritance/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    //create a class called Person 
+    public class Person
+    {
+        //give it two properties each of data type string.
+        //one called FirstName, the other LastName.
+        public string FirstName;
+        public string LastName;
+
+        //give the class a void method called SayName() that takes no parameters 
+        //and simply writes the person's full name to the console in the format of: "Name: [full name]".
+        public void SayName()
+        {
+            Console.WriteLine("Name:{0} {1}", FirstName, LastName);
+        }
+    }
+
+
+}
---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate and initialize an Employee object with a first name of "Sample" and a last name of "Student".
+            Employee firstEmployee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            //call the superclass method SayName() on the Employee object.
+            firstEmployee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
