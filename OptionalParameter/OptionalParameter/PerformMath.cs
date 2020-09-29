e65698d746e1c03756869f0dcae16e4dd86617e2
gracearnsberger <50274639+gracearnsberger@users.noreply.github.com>
2020-09-28 19:23:57 -0600

    Optional Parameter Practice
    Ask for user input for 1 manadatory and 1 optional number to perform math operations and display the answer to the console using c# methods and classes.

---
+++ OptionalParameter/OptionalParameter/PerformMath.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameter
+{
+    class PerformMath
+    {
+        //in your class, create a method that takes two integers as parameters. make one of them optional. have the method do a math operation. then display the result.
+        public int MyMath(int int1, int int2 = 10)
+        {
+            return int1 + int2;
+        }
+    }
+}
---
+++ OptionalParameter/OptionalParameter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the class.
+            PerformMath operation = new PerformMath();
+
+            //ask the user to input two numbers, one at a time. let them know they need not enter anything for the second number.
+            Console.WriteLine("Enter a number. You may enter a second number " +
+                "if you would like or you may leave the second space blank.");
+
+            //call the method in the class, passing in the one or two numbers entered.
+            int int1 = Convert.ToInt32(Console.ReadLine());
+            string input = Console.ReadLine();
+            int int2;
+
+            if (input == "")
+            {
+                int int3 = operation.MyMath(int1);
+                Console.WriteLine("Here is your final number after the math operations were performed: " + int3);
+            }
+            else
+            {
+                int2 = Convert.ToInt32(input);
+                int int3 = operation.MyMath(int1, int2);
+                Console.WriteLine("Here is your final number after the math operations were performed: " + int3);
+            }
+            Console.ReadLine();
+        }
+    }
+}
