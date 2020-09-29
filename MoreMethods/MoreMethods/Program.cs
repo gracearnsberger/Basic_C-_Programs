d6a4ae7524a1b074156e3aff006dedd18882226d
gracearnsberger <50274639+gracearnsberger@users.noreply.github.com>
2020-09-28 18:50:05 -0600

    More Methods
    Practice instantiating classes and using methods to perform math operations.

---
+++ MoreMethods/MoreMethods/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the class and call the one method, passing in an integer. Display the result to the screen.
+            int x = 2;
+            int Total;
+            MyMethods intFunction = new MyMethods();
+            Total = intFunction.OperationOne(x);
+            Console.WriteLine("6 times 2 is: ");
+            Console.WriteLine(Total);
+
+            //instantiate the class and call the second method, passing in a decimal. Display the result to the screen.
+            decimal y = 23.25m;
+            int Answer;
+            MyMethods decimalFunction = new MyMethods();
+            Answer = decimalFunction.OperationOne(y);
+            Console.WriteLine("23.25 minus 0.05 is: ");
+            Console.WriteLine(Answer);
+
+            //instantiate the class and call the third method, passing in a string that equates to an integer. Display the result to the screen.
+            string z;
+            int number;
+            MyMethods stringFunction = new MyMethods();
+            Console.WriteLine("Enter a number");
+            z = Console.ReadLine();
+            Console.WriteLine(z + " plus 4 is: ");
+            number = stringFunction.OperationOne(z);
+            Console.WriteLine(number);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MoreMethods/MoreMethods/mymethods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreMethods
+{
+    public class MyMethods
+    {
+        //in a class, create a method that will take in an integer
+        public int OperationOne(int int1)
+        {
+            int int2 = 6;
+            var Total = int1 * int2;
+            return Total;
+        }
+        //second method with the same name that will take in a decimal, create a different math operation for it
+        public int OperationOne(decimal int1)
+        {
+            decimal int2 = 0.05m;
+            var Answer = Convert.ToInt32(int1 - int2);
+            return Answer;
+        }
+        //third method to the class, with the same name, that will take in a string & do a different math operation to it.
+        public int OperationOne(string string1)
+        {
+            int int3 = 4;
+            int int4 = Convert.ToInt32(string1);
+            int number = int4 + int3;
+            return number;
+        }
+    }
+}
