b978f10e9d66050000e09eb83b74d7d64562c013
gracearnsberger <50274639+gracearnsberger@users.noreply.github.com>
2020-09-30 09:22:47 -0600

    Abstract Class
    Practice using abstract classes in c#.

---
+++ AbstractClass/AbstractClass/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    //create an abstract class called Person 
+    public abstract class Person
+    {
+        //two properties: string firstName and string lastName
+        public string firstName;
+        public string lastName;
+
+        //give it the method SayName()
+        public virtual string SayName()
+        {
+            Console.WriteLine("Name: {0} {1}", firstName, lastName);
+            return null;
+        }
+    }
+}
---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //inside the Main() method, instantiate an Employee object with firstName "Sample" and lastName "Student". 
+            Employee employee = new Employee() { firstName = "Sample", lastName = "Student" };
+
+            //call the SayName() method on the object.
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
