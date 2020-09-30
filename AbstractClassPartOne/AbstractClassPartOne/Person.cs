e24baf65ff25f047186b85071f7ec72519c3a8aa
gracearnsberger <50274639+gracearnsberger@users.noreply.github.com>
2020-09-30 14:22:12 -0600

    Abstract Class Assignment

---
+++ AbstractClassPartOne/AbstractClassPartOne/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassPartOne
+{
+    public abstract class Person
+    {
+        //create an abstract class called Person with two properties: 
+        //string firstName and string lastName
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        //give it the method SayName()
+        public abstract void SayName();
+    }
+}
---
+++ AbstractClassPartOne/AbstractClassPartOne/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassPartOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //inside the Main() method, instantiate an Employee object
+            //with firstName "Sample" and lastName "Student". 
+            Employee employee = new Employee();
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+
+            //call the SayName() method on the object.
+            employee.SayName();
+
+            Console.ReadLine();
+
+        }
+    }
+}
