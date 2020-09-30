ff28130775f26c12a31d81449b0dc1c97d9de644
gracearnsberger <50274639+gracearnsberger@users.noreply.github.com>
2020-09-30 15:42:53 -0600

    Overload Operator
    overload the "==" operator so it checks if two Employee objects are equal by comparing their Id property.

---
+++ OverLoadOperator/OverLoadOperator/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverLoadOperator
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        //overload the "==" operator so it checks if two Employee objects are equal by comparing their Id property.
+        public static bool operator == (Employee employeeOne, Employee employeeTwo)
+        {
+            if (employeeOne.Id == employeeTwo.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator != (Employee employeeOne, Employee employeeTwo)
+        {
+            if (employeeOne.Id != employeeTwo.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ OverLoadOperator/OverLoadOperator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverLoadOperator
+{
+    //overload the "==" operator so it checks if two Employee objects are equal by comparing their Id property.
+    class Program : Employee
+    {
+        static void Main(string[] args)
+        {
+            Employee employeeOne = new Employee() { Id = 1, Name = "Joe" };
+            Employee employeeTwo = new Employee() { Id = 1, Name = "Michael" };
+
+            bool sameId = employeeOne == employeeTwo;
+
+            Console.WriteLine("Do " + employeeOne.Name + " and " + employeeTwo.Name + "'s Id's match? " + sameId);
+            Console.ReadLine();
+        }
+    }
+}
