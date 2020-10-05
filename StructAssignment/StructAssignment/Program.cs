ca47e9ccd912b0672637755d6f03b2fd5ae31a97
gracearnsberger <50274639+gracearnsberger@users.noreply.github.com>
2020-10-05 11:04:53 -0600

    Struct Assignment

---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //in the Main() method, create an object of data type Number and assign an amount to it.
+            Number myNumber = new Number();
+            myNumber.Amount = 52.26m;
+
+            //print this amount to the console.
+            Console.WriteLine(myNumber.Amount);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StructAssignment/StructAssignment/Struct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    //create a struct called Number 
+    public struct Number
+    {
+        //give it the property "Amount" and have it be of data type decimal.
+        public decimal Amount { get; set; }
+    }
+}
