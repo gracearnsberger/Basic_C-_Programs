d0071e1cad86ec6dd46d41709cd76b54c28bed58
gracearnsberger <50274639+gracearnsberger@users.noreply.github.com>
2020-10-12 12:17:27 -0600

    File IO Assignment

---
+++ FileIOAssignment/FileIOAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIOAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //asks the user for a number
+            Console.WriteLine("Please enter a number: ");
+            string num = string.Format(Console.ReadLine());
+
+            //log that number to a text file
+            File.WriteAllText(@"C:\G\numberlog.txt", num);
+
+            //prints the text file back to the user
+            string display = File.ReadAllText(@"C:\G\numberlog.txt");
+            Console.WriteLine(display);
+
+            Console.ReadLine();
+        }
+    }
+}
